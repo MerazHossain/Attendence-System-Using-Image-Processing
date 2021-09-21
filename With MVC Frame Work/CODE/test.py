27f39196fe18327fb7ed8aee1980d993219067c6
unknown <meraz.rahat@gmail.com>
2021-09-21 08:35:45 -0700

    First Commit

---
+++ With MVC Frame Work/CODE/test.py
@@ -0,0 +1,17 @@
+import unittest
+
+class LearnTest(unittest.TestCase):
+    def test_myfunc(self):
+        pass
+   
+    def test_func_1 (self):
+        pass
+    def test_func_2 (self):
+        pass
+    def test_func_3 (self):
+        pass
+    def test_func_4 (self):
+        pass
+
+if __name__=="__main__":
+    unittest.main()
---
+++ test.py
@@ -0,0 +1,8 @@
+import unittest
+
+class LearnTest(unittest.TestCase)
+    def test_func_1 (self):
+        pass
+
+if _name_=="_main_"
+    unittest.main
